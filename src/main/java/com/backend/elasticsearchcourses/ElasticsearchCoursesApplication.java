571bad11c5779f2cc84d832ceaee6f93e2a4d8a5
shivanshow <singhshivanshu337@gmail.com>
2025-08-30 22:31:10 +0530

    Add Spring Boot main application class

---
+++ src/main/java/com/backend/elasticsearchcourses/ElasticsearchCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.backend.elasticsearchcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchCoursesApplication.class, args);
+    }
+
+}
